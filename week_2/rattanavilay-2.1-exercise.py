1fb83be89577fcb705d83d82e5d41229568c7bad
Thip Rattanavilay <trattanavilay@my365.bellevue.edu>
2020-12-13 00:03:41 -0800

    Create rattanavilay-2.1-exercise.py

---
+++ week_2/rattanavilay-2.1-exercise.py
@@ -0,0 +1,47 @@
+# -------------------------------------------------
+# File: rattanavilay-2.1-exercise.py
+# Name: Thip Rattanavilay
+# Assignment: 2.1 Exercise: Preparing for Exploratory Data Analysis
+# Date: 12/6/20
+# Course: DSC 530 – EDA
+# Description: This week’s exercise is to start getting prepared for the
+# exploratory data analysis we will be doing in this course by working in Python.
+# -------------------------------------------------
+
+# Display the text “Hello World! I wonder why that is always the default
+# coding text to start with”
+print('Hello World! I wonder why that is always the default coding text to start with.')
+
+# Add two numbers together
+add = 300 + 65
+print(add)
+
+# Subtract a number from another number
+subtract = 2020 - 1983
+print(subtract)
+
+# Multiply two numbers
+multiply = 50 * 60
+print(multiply)
+
+# Divide between two numbers
+divide = 300 / 65
+print(divide)
+
+# Concatenate two strings together (any words)
+word1 = 'Happy'
+word2 = 'NewYear'
+happy_new_year = word1 + word2
+print(happy_new_year)
+
+# Create a list of 4 items (can be strings, numbers, both)
+party_items = ["Champagne", "Chips & Dips", "Balloons", "Confetti"]
+print(party_items)
+
+# Append an item to your list (again, can be a string, number)
+party_items.append("12:00 AM")
+print(party_items)
+
+# Create a tuple with 4 items (can be strings, numbers, both)
+party_time = ("Dance", 'Music', 'People', 2021)
+print(party_time)
